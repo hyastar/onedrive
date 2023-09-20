424a8bbfe197c7dbf7ff99ebc7a1754861471b3a
hyastar <alpha.hydrae.zero@gmail.com>
2023-09-20 15:38:34 +0800

    11

--- config/site.config.js
+++ config/site.config.js
@@ -39,7 +39,7 @@ module.exports = {
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
 
-    'Powered by <a href="https://hydrae.cc" target="_blank" rel="noopener noreferrer">Houanxin&#39;netdisc</a>. Made with ❤ by Houanxin.'
+    'Powered by <a href="https://hydrae.cc" target="_blank" rel="noopener noreferrer">Houanxin&#39;netdisc</a>. Made with ❤ by Houanxin.',
  
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
